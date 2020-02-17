45eaf4164715e9f737c172c15eb5b9d0d0e8cb48
Liam <22pilarskil@millburn.org>
2020-02-17 16:56:12 -0700

    adding django_heroku

--- EssexIQ/settings.py
+++ EssexIQ/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/2.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
-
+import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -139,6 +139,8 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+django_heroku.settings(locals())
 
 
 # Static files (CSS, JavaScript, Images)
