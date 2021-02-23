4f04326a9fc83a2a861fee6220447e13aaa1058d
Liam <22pilarskil@millburn.org>
2021-02-22 23:55:30 -0500

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,8 @@
 from . import views
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 app_name = 'main'
 
@@ -10,4 +13,4 @@ urlpatterns = [
     path('winter-session', views.return_view, name='winter'),
     path('spring-session', views.return_view, name='spring'),
     path('summer-session', views.return_view, name="summer"),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
